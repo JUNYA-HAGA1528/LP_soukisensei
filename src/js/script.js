b7ecec1a913e5159535a65affd7b64df3fb961e1
junyahaga <n0812034@yahoo.co.jp>
2024-01-06 23:54:42 +0900

    2024/1/6

--- src/js/script.js
+++ src/js/script.js
@@ -1,69 +1,34 @@
-  ////////////
-  // コンタクト 未入力エラー
-  ////////////
 
-  $(document).ready(function () {
-    $(".js-submit").on("click", function (event) {
-      var form = document.getElementById("form");
-      var requiredElements = form.querySelectorAll("[required]");
-      var hasRequiredFields = false;
-      var inquiryCheckboxes = form.querySelectorAll(
-        'input[name="お問合せ項目"]'
-      );
+$(document).ready(function(){
+  $('#form').submit(function(event){
+      var error = false;
 
-      // 必須項目のクラスとチェックボックスの選択状態をチェック
-      requiredElements.forEach(function (element) {
-        if (element.type === "checkbox") {
-          if (!element.checked) {
-            element.classList.add("required");
-            hasRequiredFields = true;
-          } else {
-            element.classList.remove("required");
-          }
-        } else {
-          if (element.value.trim() === "") {
-            element.classList.add("required");
-            hasRequiredFields = true;
-          } else {
-            element.classList.remove("required");
-          }
-        }
-      });
-
-      // チェックボックスの選択状態をチェック
-      var isAnyCheckboxSelected = false;
-      inquiryCheckboxes.forEach(function (checkbox) {
-        if (checkbox.checked) {
-          isAnyCheckboxSelected = true;
-        }
-      });
-
-      // チェックボックスの選択状態に応じて.requiredクラスを制御
-      inquiryCheckboxes.forEach(function (checkbox) {
-        if (!isAnyCheckboxSelected) {
-          checkbox.classList.add("required");
-          hasRequiredFields = true;
-        } else {
-          checkbox.classList.remove("required");
-        }
-      });
-
-      // .requiredが一つでもあるかをチェック
-      var hasAnyRequiredField = form.querySelector(".required") !== null;
-
-      // .sub-contact__errorの表示制御
-      var errorElement = $(".sub-contact__error");
-      errorElement.toggleClass("active", hasAnyRequiredField);
-
-      // .breadcrumb__item--errorの表示制御
-      var breadcrumbErrorElement = $(".breadcrumb__item--error");
-      breadcrumbErrorElement.toggleClass("active", hasAnyRequiredField);
-
-      if (hasAnyRequiredField) {
-        event.preventDefault(); // バリデーションエラー時はデフォルトの送信を阻止
-      } else {
-        // 未入力項目がない場合の処理
-        window.location.href = "contact-thanks.html"; // ページ遷移
+      // ①性別
+      if ($('input[name="sex"]:checked').length === 0) {
+          error = true;
       }
-    });
+
+      // ②生年月日
+      if ($('#BirthYear').val() === '' || $('#BirthMonth').val() === '' || $('#BirthDay').val() === '') {
+          error = true;
+      }
+
+      // ③名前
+      if ($('input[name="content"]').val() === '') {
+          error = true;
+      }
+
+      // ④メールアドレス
+      if ($('input[name="email"]').val() === '') {
+          error = true;
+      }
+
+      if (error) {
+          $('.contact__error').slideDown(); // エラーメッセージ表示
+          event.preventDefault(); // フォームの送信を防止
+      } else {
+          $('.contact__error').slideUp(); // エラーメッセージ非表示
+      }
   });
+});
+
