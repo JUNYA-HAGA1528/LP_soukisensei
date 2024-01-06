8333112b6d3896e790ccf24075d4b192614c310e
junyahaga <n0812034@yahoo.co.jp>
2024-01-07 01:36:49 +0900

    2024/1/6

--- src/js/script.js
+++ src/js/script.js
@@ -1,34 +1,33 @@
 
 $(document).ready(function(){
-  $('#form').submit(function(event){
-      var error = false;
+    $('#form').submit(function(event){
+        var error = false;
 
-      // ①性別
-      if ($('input[name="sex"]:checked').length === 0) {
-          error = true;
-      }
+        // ①性別
+        if ($('input[name="sex"]:checked').length === 0) {
+            error = true;
+        }
 
-      // ②生年月日
-      if ($('#BirthYear').val() === '' || $('#BirthMonth').val() === '' || $('#BirthDay').val() === '') {
-          error = true;
-      }
+        // ②生年月日
+        if ($('#BirthYear').val() === '' || $('#BirthMonth').val() === '' || $('#BirthDay').val() === '') {
+            error = true;
+        }
 
-      // ③名前
-      if ($('input[name="content"]').val() === '') {
-          error = true;
-      }
+        // ③名前
+        if ($('input[name="content"]').val() === '') {
+            error = true;
+        }
 
-      // ④メールアドレス
-      if ($('input[name="email"]').val() === '') {
-          error = true;
-      }
+        // ④メールアドレス
+        if ($('input[name="email"]').val() === '') {
+            error = true;
+        }
 
-      if (error) {
-          $('.contact__error').slideDown(); // エラーメッセージ表示
-          event.preventDefault(); // フォームの送信を防止
-      } else {
-          $('.contact__error').slideUp(); // エラーメッセージ非表示
-      }
-  });
+        if (error) {
+            $('.contact__error').slideDown(); // エラーメッセージ表示
+            event.preventDefault(); // フォームの送信を防止
+        } else {
+            window.location.href = 'contact-check.html';
+        }
+    });
 });
-
